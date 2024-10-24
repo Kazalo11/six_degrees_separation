fb6b14fb46613da53a26baeb9429c75bf81feb24
Kazal Oshodi <kazoshodi@gmail.com>
2024-10-25 00:47:28 +0100

    fix stuff

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"log"
-
 	"github.com/Kazalo11/six-degrees-seperation/internal/server"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	server.Start()
 }
