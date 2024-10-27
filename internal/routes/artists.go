031abc949638369da10b27823d9a90aba1070985
Kazal Oshodi <kazoshodi@gmail.com>
2024-10-27 20:21:00 +0000

    clean up

--- internal/routes/artists.go
+++ internal/routes/artists.go
@@ -1,9 +1,7 @@
 package routes
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -99,33 +97,10 @@ func upsertGraph(feat albumFuncs.FeaturedArtistInfo, direction artist.Direction,
 			log.Printf("Could not find featured artists for %s due to err: %v", newArtist.Name, err2)
 			continue
 		}
-		g = artist.UpsertGraph(newFeat, idx, direction, g)
+		graph = artist.UpsertGraph(newFeat, idx, direction, graph)
 
 	}
-	return g
-}
-
-func featuredArtistInfo(id string) (albumFuncs.FeaturedArtistInfo, error) {
-	var feat albumFuncs.FeaturedArtistInfo
-	domain := os.Getenv("DOMAIN")
-	resp1, err := http.Get(fmt.Sprintf("%s/v1/artist/%s/features", domain, id))
-	if err != nil {
-		log.Printf("Failed to get features for artist: %s", id)
-		return nil, err
-	}
-	body, err := io.ReadAll(resp1.Body)
-	if err != nil {
-		log.Printf("Failed to read response body for artist: %s", id)
-		return feat, err
-	}
-
-	err = json.Unmarshal(body, &feat)
-	if err != nil {
-		log.Printf("Failed to decode json for artist: %s", id)
-		return feat, err
-	}
-	return feat, nil
-
+	return graph
 }
 
 func getFeaturedArtists(c *gin.Context) {
